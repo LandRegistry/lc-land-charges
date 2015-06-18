61653bbbe0339007e612e7539d76c2826861ceb5
Ian Harvey <ian.harvey@digital.landregistry.gov.uk>
2015-06-18 07:50:36 +0100

    Add config & requirements

--- application/__init__.py
+++ application/__init__.py
@@ -1,5 +1,6 @@
 from flask import Flask
 
 app = Flask(__name__)
+app.config.from_object(os.environ.get('SETTINGS'))
 
 from application import routes
---
+++ config.py
@@ -0,0 +1,7 @@
+import os
+
+class Config(object):
+    DEBUG = False
+
+class DevelopmentConfig(object):
+    DEBUG = True
