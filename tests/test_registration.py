5a6a748e65cef7239e3fa2c9efed56147d17b6f8
Ian Harvey <ian.harvey@digital.landregistry.gov.uk>
2015-07-07 11:48:44 +0100

    Update tests to reflect amended API

--- tests/test_registration.py
+++ tests/test_registration.py
@@ -9,7 +9,7 @@ class MockConnection:
         self.results = results
 
     def cursor(self):
-        return MockCursor(self.results)
+        return MockCursor(self.results, self)
 
     def commit(self):
         pass
@@ -19,8 +19,9 @@ class MockConnection:
 
 
 class MockCursor:
-    def __init__(self, results):
+    def __init__(self, results, conn):
         self.results = results
+        self.connection = conn
 
     def execute(self, sql, dict):
         pass
@@ -31,8 +32,12 @@ class MockCursor:
     def fetchall(self):
         return self.results
 
+    def fetchone(self):
+        return [1]
+
 dir = os.path.dirname(__file__)
 valid_data = open(os.path.join(dir, 'data/valid_data.json'), 'r').read()
+name_data = '{"forenames": "Bob Oscar Francis", "surname": "Howard"}'
 mock_connection = MockConnection([valid_data])
 mock_empty_connection = MockConnection([])
 
@@ -46,12 +51,14 @@ class TestWorking:
 
     @mock.patch('psycopg2.connect', return_value=mock_connection)
     def test_item_found(self, mock_connect):
-        response = self.app.get('/search/222')
+        headers = {'Content-Type': 'application/json'}
+        response = self.app.post('/search', data=name_data, headers=headers)
         assert response.status_code == 200
 
     @mock.patch('psycopg2.connect', return_value=mock_empty_connection)
     def test_item_not_found(self, mock_connect):
-        response = self.app.get('/search/222')
+        headers = {'Content-Type': 'application/json'}
+        response = self.app.post('/search', data=name_data, headers=headers)
         assert response.status_code == 404
 
     @mock.patch('psycopg2.connect', return_value=mock_connection)
@@ -64,7 +71,7 @@ class TestWorking:
     def test_new_registration(self, mock_connect):
         headers = {'Content-Type': 'application/json'}
         response = self.app.post('/register', data=valid_data, headers=headers)
-        assert response.status_code == 202
+        assert response.status_code == 200
 
     @mock.patch('psycopg2.connect', side_effect=Exception('Fail'))
     def test_database_failure(self, mock_connect):
