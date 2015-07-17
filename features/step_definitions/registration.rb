b264780a7b1e677b6eca3c81669e61b04fcb47b8
Ian Harvey <ian.harvey@digital.landregistry.gov.uk>
2015-07-17 14:07:44 +0100

    Acceptance testing incl. syncroniser

--- features/step_definitions/registration.rb
+++ features/step_definitions/registration.rb
@@ -4,6 +4,8 @@ require 'rspec'
 require 'pg'
 
 no_alias = '{"key_number":"9056267","application_type":"PA(B)","application_ref":"9763603","date":"2014-11-12","debtor_name":{"forenames":["Lamar","Sigmund"],"surname":"Effertz"},"debtor_alternative_name":[],"gender":"N/A","occupation":"Ship builder","residence":[{"address_lines":["942 Carley Unions","Cullenberg","Dimitrimouth","Buckinghamshire"],"postcode":"QF47 0HG"}],"residence_withheld":false,"business_address":{"address_lines":["122 Leuschke Creek","Alvaburgh","Fife"],"postcode":"NO03 1EU"},"date_of_birth":"1974-10-03","investment_property":[]}'
+one_alias = '{"key_number":"6269524","application_type":"PA(B)","application_ref":"7282631","date":"2015-02-19","debtor_name":{"forenames":["Helga","Nelda"],"surname":"Hessel"},"debtor_alternative_name":[{"forenames":["Nelda","Helga"],"surname":"Hessel"}],"gender":"N/A","occupation":"Flower arranger","residence":[{"address_lines":["45633 Wyman Corner","Huelshire","Lake Jennings","West Yorkshire"],"postcode":"YL23 2FD"}],"residence_withheld":false,"business_address":{"address_lines":["423 Zander Mount","Konopelskifurt","South Shyannberg","Shropshire"],"postcode":"AE64 7DF"},"date_of_birth":"1953-01-11","investment_property":[]}'
+no_alias_alt = '{"key_number":"1479067","application_type":"PA(B)","application_ref":"9045789","date":"2014-10-28","debtor_name":{"forenames":["Wilfredo","Bernice"],"surname":"Ernser"},"debtor_alternative_name":[],"gender":"N/A","occupation":"Bookmaker","residence":[{"address_lines":["1940 Huels Fort","North Glennamouth","South Nonafort","West Yorkshire"],"postcode":"PA85 4RH"}],"residence_withheld":false,"business_address":{"address_lines":["831 Hailee Burg","Chasityborough","East Tamara","Northamptonshire"],"postcode":"IP81 2CM"},"date_of_birth":"1974-10-07","investment_property":[]}'
 
 class AssertionFailure < RuntimeError
 end
@@ -32,8 +34,8 @@ class RestAPI
 end
 
 class PostgreSQL
-	def self.connect
-		@@pg = PGconn.connect( 'localhost', 5432,  '', '', 'landcharges', 'vagrant', 'vagrant')
+	def self.connect(database)
+		@@pg = PGconn.connect( 'localhost', 5432,  '', '', database, 'vagrant', 'vagrant')
 	end
 	
 	def self.disconnect
@@ -67,10 +69,41 @@ Then(/^it returns the new registration number$/) do
 end
 
 Then(/^a new record is stored on the database$/) do
-	PostgreSQL.connect
+	PostgreSQL.connect('landcharges')
 	registration_api.data["new_registrations"].each do |reg_no|		
 		result = PostgreSQL.query("SELECT * FROM register WHERE registration_no=#{reg_no}")
 		expect(result.values.length).to be 1		
+	end
+	PostgreSQL.disconnect
+end
+
+When(/^I submit valid data with an alias to the registration system$/) do
+	registration_api = RestAPI.new("http://localhost:5004")
+	registration_api.post_data("/register", one_alias)
+end
+
+Then(/^it returns the (\d+) new registration numbers$/) do |arg1|
+	assert(registration_api.data["new_registrations"].length.to_s == arg1)
+end
+
+Then(/^(\d+) new records are stored on the database$/) do |arg1|
+	PostgreSQL.connect('landcharges')
+	c = 0
+	registration_api.data["new_registrations"].each do |reg_no|		
+		result = PostgreSQL.query("SELECT * FROM register WHERE registration_no=#{reg_no}")
+		expect(result.values.length).to be 1		
+		c += 1
+	end
+	expect(c.to_s).to eql arg1
+	PostgreSQL.disconnect
+end
+
+Then(/^the data is recorded on DB2$/) do
+	sleep(1)
+	PostgreSQL.connect('db2')
+	registration_api.data["new_registrations"].each do |reg_no|		
+		result = PostgreSQL.query("SELECT * FROM lc_mock WHERE registration_no='#{reg_no}'")
+		expect(result.values.length).to eq 1		
 	end
 	PostgreSQL.disconnect
 end
