7bf7eb51a23212aef598f97ef974d6c7dd1c143e
Ian Harvey <ian.harvey@digital.landregistry.gov.uk>
2015-09-30 07:44:09 +0100

    Set exit code

--- test.sh
+++ test.sh
@@ -5,6 +5,7 @@ if [ "$1" != "CI" ]; then
     py.test --cov application tests/ --cov-report=term --cov-report=html --junit-xml=junit.xml
 else
     test="$(py.test --cov application tests/ --cov-report=term --cov-report=html --junit-xml=junit.xml)"
+    code=$?
     echo "${test}"
     cov=`echo ${test} | perl -nle 'm/TOTAL\s+\d+\s+\d+\s+(\d+)%/; print $1'`
 
@@ -13,4 +14,5 @@ else
     else
         echo "Coverage OK!"
     fi
+    exit $code
 fi
