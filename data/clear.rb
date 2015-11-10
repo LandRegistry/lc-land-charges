65543dc04f480bc0d9736e7755992b913e5236a8
Ian Harvey <ian.harvey@digital.landregistry.gov.uk>
2015-11-10 13:56:39 +0000

    Ruby data scripts

---
+++ data/clear.rb
@@ -0,0 +1,7 @@
+require 'net/http'
+require 'json'
+
+uri = URI(ENV['BANKRUPTCY_REGISTRATION_URI'] || 'http://localhost:5004')
+http = Net::HTTP.new(uri.host, uri.port)
+
+response = http.request(Net::HTTP::Delete.new('/registrations'))
--- data/delete.py
+++
@@ -1,5 +0,0 @@
-import os
-import requests
-
-url = os.getenv('LAND_CHARGES_URI', 'http://localhost:5004')
-requests.delete(url + '/registrations')
