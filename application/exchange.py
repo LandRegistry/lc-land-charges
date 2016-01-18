8e05e12bef58cf80c2698b14f241b9aa9e0a39e2
Ian Harvey <ian.harvey@digital.landregistry.gov.uk>
2016-01-18 13:33:33 +0000

    Don't bother with queues if MQ_USERNAME isn't specified [Interim?]

--- application/exchange.py
+++ application/exchange.py
@@ -5,42 +5,49 @@ import logging
 
 
 def setup_messaging(config):
-    host = "amqp://{}:{}@{}:{}".format(config['MQ_USERNAME'], config['MQ_PASSWORD'], config['MQ_HOSTNAME'],
-                                       config['MQ_PORT'])
-    logging.debug("Connect to " + host)
-    connection = kombu.Connection(hostname=host)
-    channel = connection.channel()
-    exchange = kombu.Exchange(type="topic", name="new.bankruptcy")
-    exchange.maybe_bind(channel)
-    maybe_declare(exchange, channel)
-    prod = kombu.Producer(channel, exchange=exchange, routing_key='simple', serializer='json')
-    logging.info("Producer started")
-    sys.stdout.flush()
-    return prod
+    if 'MQ_USERNAME' in config:
+        host = "amqp://{}:{}@{}:{}".format(config['MQ_USERNAME'], config['MQ_PASSWORD'], config['MQ_HOSTNAME'],
+                                           config['MQ_PORT'])
+        logging.debug("Connect to " + host)
+        connection = kombu.Connection(hostname=host)
+        channel = connection.channel()
+        exchange = kombu.Exchange(type="topic", name="new.bankruptcy")
+        exchange.maybe_bind(channel)
+        maybe_declare(exchange, channel)
+        prod = kombu.Producer(channel, exchange=exchange, routing_key='simple', serializer='json')
+        logging.info("Producer started")
+        sys.stdout.flush()
+        return prod
+    else:
+        logging.warning("No publication exchange created.")
+        return None
 
 
 def publish_new_bankruptcy(producer, data):
-    publish = {
-        "application": "new",
-        "data": data
-    }
-    logging.info("Sending: %s", publish)
-    producer.publish(publish)
+    if producer is not None:
+        publish = {
+            "application": "new",
+            "data": data
+        }
+        logging.info("Sending: %s", publish)
+        producer.publish(publish)
 
 
 def publish_amendment(producer, data):
-    publish = {
-        "application": "amend",
-        "data": data
-    }
-    logging.info("Sending: %s", publish)
-    producer.publish(publish)
+    if producer is not None:
+        publish = {
+            "application": "amend",
+            "data": data
+        }
+        logging.info("Sending: %s", publish)
+        producer.publish(publish)
 
 
 def publish_cancellation(producer, data):
-    publish = {
-        "application": "cancel",
-        "data": data
-    }
-    logging.info("Sending: %s", publish)
-    producer.publish(publish)
+    if producer is not None:
+        publish = {
+            "application": "cancel",
+            "data": data
+        }
+        logging.info("Sending: %s", publish)
+        producer.publish(publish)
