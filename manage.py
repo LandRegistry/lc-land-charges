48b9933157242f5f85b60453e23f33d275be86eb
Ian Harvey <ian.harvey@digital.landregistry.gov.uk>
2016-02-24 10:25:45 +0000

    New table

--- manage.py
+++ manage.py
@@ -5,7 +5,7 @@ from flask.ext.sqlalchemy import SQLAlchemy
 import os
 
 app = Flask(__name__)
-app.config.from_object(os.getenv('SETTINGS', 'config.DevelopmentConfig'))
+app.config.from_object('config.Config')
 
 db = SQLAlchemy(app)
 migrate = Migrate(app, db)
