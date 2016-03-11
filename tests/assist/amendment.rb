19a6c56244292350d373a0741bccdcb36b311ae4
Ian Harvey <ian.harvey@digital.landregistry.gov.uk>
2016-03-11 08:46:17 +0000

    Logging change

--- tests/assist/amendment.rb
+++ tests/assist/amendment.rb
@@ -40,26 +40,23 @@ wob_1 = '{"parties": [{"type": "Debtor", "legal_body_ref_no": "123 of 2016", "tr
 
 amendment = '"parties": [{"residence_withheld": false, "trading_name": " ", "legal_body_ref_no": "123 of 2016", "legal_body": "Devon County Court", "type": "Debtor", "case_reference": "Devon County Court 123 of 2016", "addresses": [{"type": "Residence", "postcode": "Blah", "county": "Blah", "address_lines": ["1 Other Road", "Blah"], "address_string": "1 Other Road Blah Blah Blah"}], "names": [{"type": "Private Individual", "private": {"forenames": ["Mister"], "surname": "Bankrupt"}}], "occupation": "Truck Driver"}], "class_of_charge": "WOB", "update_registration": {"type": "Amendment"}, "applicant": {"key_number": "1234567", "name": "S & H Legal Group", "address": "49 Camille Circles Port Eulah PP39 6BY", "reference": " "}}'
 
-
-# wob_regn_1_name = '{"applicant": {"name": "Waste of space", "address": "2 New Street, My Town", "key_number": "1234567", "reference": " "}, "parties":[{"names": [{"type": "Private Individual", "private": {"forenames": ["John", "Alan"], "surname": "Smithe"}}], "trading_name": " ", "addresses": [{"county": "Devon", "address_lines": ["2 new street", "Plymouth"], "postcode": "PL3 3PL", "type": "Residence", "address_string": "2 new street Plymouth Devon PL3 3PL"}, {"county": "Dorset", "address_lines": ["3 apple Street", "plymouth", "a third line", "a fourth line", "a five line"], "postcode": "postcode", "type": "Residence", "address_string": "3 apple Street plymouth a third line a fourth line a five line Cornwall postcode"}], "occupation": "", "type": "Debtor", "residence_withheld": false, "case_reference": "Plympton County Court 111 of 2016", "legal_body": "Plympton County Court", "legal_body_ref_no": "111 of 2016", "legal_body_ref_year": 2016, "counties": ["Devon", "Dorset"]}], "class_of_charge": "WOB"}'
-
-
-# correction_1_name = '{"update_registration": {"type": "Amendment"}, "class_of_charge": "WOB", "applicant": {"reference": " ", "name": "Waste of space", "key_number": "1234567", "address": "2 New Street, My Town"}, "parties": [{"case_reference": "Plympton County Court 111 of 2016", "addresses": [{"county": "Devon", "postcode": "PL3 3PL", "type": "Residence", "address_lines": ["2 new street", "Plymouth"], "address_string": "2 new street Plymouth Devon PL3 3PL"}, {"county": "Dorset", "postcode": "postcode", "type": "Residence", "address_lines": ["3 apple Street", "plymouth", "a third line", "a fourth line", "a five line"], "address_string": "3 apple Street plymouth a third line a fourth line a five line Dorset postcode"}], "legal_body_ref_no": "111 of 2016", "occupation": "Lunatic", "residence_withheld": false, "trading_name": " ", "type": "Debtor", "legal_body": "Plympton County Court", "names": [{"type": "Private Individual", "private": {"forenames": ["John", "Alan"], "surname": "Smithe"}}]}]}'
-
-
-# wob_regn_2_names = '{"applicant": {"name": "Waste of space", "address": "2 New Street, My Town", "key_number": "1234567", "reference": " "}, "parties":[{"names": [{"type": "Private Individual", "private": {"forenames": ["John"], "surname": "Smith" }}, {"type": "Private Individual", "private": {"forenames": ["John", "Alan"], "surname": "Smithe"}}], "trading_name": " ", "addresses": [{"county": "Devon", "address_lines": ["2 new street", "Plymouth"], "postcode": "PL3 3PL", "type": "Residence", "address_string": "2 new street Plymouth Devon PL3 3PL"}, {"county": "Dorset", "address_lines": ["3 apple Street", "plymouth", "a third line", "a fourth line", "a five line"], "postcode": "postcode", "type": "Residence", "address_string": "3 apple Street plymouth a third line a fourth line a five line Cornwall postcode"}], "occupation": "", "type": "Debtor", "residence_withheld": false, "case_reference": "Plympton County Court 111 of 2016", "legal_body": "Plympton County Court", "legal_body_ref_no": "111 of 2016", "legal_body_ref_year": 2016, "counties": ["Devon", "Dorset"]}], "class_of_charge": "WOB"}'
-
-
-# correction_2_names = '{"update_registration": {"type": "Amendment"}, "class_of_charge": "WOB", "applicant": {"reference": " ", "name": "Waste of space", "key_number": "1234567", "address": "2 New Street, My Town"}, "parties": [{"case_reference": "Plympton County Court 111 of 2016", "addresses": [{"county": "Devon", "postcode": "PL3 3PL", "type": "Residence", "address_lines": ["2 new street", "Plymouth"], "address_string": "2 new street Plymouth Devon PL3 3PL"}, {"county": "Dorset", "postcode": "postcode", "type": "Residence", "address_lines": ["3 apple Street", "plymouth", "a third line", "a fourth line", "a five line"], "address_string": "3 apple Street plymouth a third line a fourth line a five line Dorset postcode"}], "legal_body_ref_no": "111 of 2016", "occupation": "Lunatic", "residence_withheld": false, "trading_name": " ", "type": "Debtor", "legal_body": "Plympton County Court", "names": [{"type": "Private Individual", "private": {"forenames": ["John"], "surname": "Smith"}}, {"type": "Private Individual", "private": {"forenames": ["John", "Alan"], "surname": "Smithe"}}]}]}'
+further_amendment = '{"parties": [{"residence_withheld": false, "trading_name": " ", "legal_body_ref_no": "123 of 2016", "legal_body": "Devon County Court", "type": "Debtor", "case_reference": "Devon County Court 123 of 2016", "addresses": [{"type": "Residence", "postcode": "Blah", "county": "Blah", "address_lines": ["1 Other Road", "Blah"], "address_string": "1 Other Road Blah Blah Blah"}], "names": [{"type": "Private Individual", "private": {"forenames": ["Mister"], "surname": "Bankrupt"}}], "occupation": "Truck Driver"}], "class_of_charge": "WOB", "update_registration": {"type": "Amendment"}, "applicant": {"key_number": "1234567", "name": "S & H Legal Group", "address": "49 Camille Circles Port Eulah PP39 6BY", "reference": " "}}'
 
 lc_api = RestAPI.new($LAND_CHARGES_URI)
 pab_reg = register(pab_1)
 wob_reg = register(wob_1)
 
 amend_data = '{"pab_amendment": {"reg_no": "' + pab_reg['number'].to_s + '", "date": "' + pab_reg['date'] + '"},' + amendment
-#puts amend_data
+
 amend = lc_api.put("/registrations/#{wob_reg['date']}/#{wob_reg['number']}", amend_data)
-puts amend
+amd_no = amend['new_registrations'][0]['number']
+amd_date = amend['new_registrations'][0]['date']
+
+furth_amend = lc_api.put("/registrations/#{amd_date}/#{amd_no}", furth_amendment)
+puts furth_amend
+
+
+# puts amend
 # pab_reg = lc_api.put("/registrations/#{pab_reg['date']}/#{pab_reg['number']}", correction_1_name)
 # puts reg2
 
@@ -67,3 +64,4 @@ puts amend
 #     puts rect['date']
 #     puts rect['number']
 # end
+# {"new_registrations"=>[{"name"=>{"type"=>"Private Individual", "private"=>{"surname"=>"Bankrupt", "forenames"=>["Mister"]}}, "number"=>1002, "date"=>"2016-03-09"}], "amended_registrations"=>[{"number"=>"1001", "date"=>"2016-03-09", "sequence"=>1}], "request_id"=>193}
