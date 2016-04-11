d8a13bc87ea0c2f148ae6c8528202dc678601d6d
Ian Harvey <ian.harvey@landregistry.gsi.gov.uk>
2016-04-11 11:32:41 +0100

    Untangle alembic

--- migrations/versions/adb741c9ee25_index_court_name.py
+++ migrations/versions/adb741c9ee25_index_court_name.py
@@ -8,7 +8,7 @@ Create Date: 2016-04-11 11:14:52.423910
 
 # revision identifiers, used by Alembic.
 revision = 'adb741c9ee25'
-down_revision = 'aff58f55852c'
+down_revision = '3d5f3d61ec72'
 branch_labels = None
 depends_on = None
 
